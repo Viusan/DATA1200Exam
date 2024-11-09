8c9f4ba1cb97a1d0756aaa16fec44694db94f963
Viusan <viusansantos@gmail.com>
2024-11-09 16:34:55 +0100

    removed stuff

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ function toggleMenu(){
     var menu = document.getElementById("nav-menu");
     if (menu.classList.contains("hidden")) {
         menu.classList.remove("hidden");
-        
     } else {
         menu.classList.add("hidden");
-        
     }
 }
