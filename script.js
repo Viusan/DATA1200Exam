e9a9ca0bac27a318f9f242b902586b4f81165b8a
Viusan <viusansantos@gmail.com>
2024-11-15 15:13:52 +0100

    I gave the hamburger menu style and some javascript so there is contrast to the colour when the button is pressed.

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function toggleMenu(){
     var menu = document.getElementById("nav-menu");
     if (menu.classList.contains("hidden")) {
         menu.classList.remove("hidden");
+        document.getElementById("menuIcon").style.color = "white"; 
     } else {
         menu.classList.add("hidden");
+        document.getElementById("menuIcon").style.color = "black"; 
+
     }
 }
